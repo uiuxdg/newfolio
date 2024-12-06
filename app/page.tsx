e4417ac71caee7744bf7aecbc3ee2a0894c4a1d2
uiuxdg <uiuxdg4@gmail.com>
2024-12-06 09:52:33 -0500

    wireframe screen

--- app/page.tsx
+++ app/page.tsx
@@ -58,6 +58,25 @@ export default function Home() {
 
               <div className="pt-8">
 
+<p className="text-neutral-800 dark:text-neutral-200 text-md md:text-lg font-normal mb-8">
+ Preliminary User flows and UI wireframing was done in Excalidraw and Figma, to plan out the architecture and design before development began.
+</p>
+<div className="grid grid-cols-1 gap-4">
+  <Image
+    src="/calcgen_wireframe.png"
+    alt="startup template"
+    width={500}
+    height={500}
+    className="rounded-lg object-contain h-auto w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+  />
+
+</div>
+</div>
+
+
+
+              <div className="pt-8">
+
               <p className="text-neutral-800 dark:text-neutral-200 text-md md:text-lg font-normal mb-8">
                 CalcGen was built in Next.js, with a custom-built AI agent framework, and uses a combination of various LLMs, structured outputs, and search to answer any data question. The frontend is styled with Tailwind CSS. CalcGen appeared as a trending product in prominent AI newsletters, including Rundown AI, Superhuman, and Superpower Daily, reaching over 2M total subscribers, and gaining over 1000 followers on instagram.
               </p>
